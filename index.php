f6ed7f117c3def3007daad2302f565004f74bb13
Benjamin Hall <benhall14@hotmail.com>
2017-04-01 13:45:35 +0100

    Removed 'Display Error' code

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require('src/php-imap-reader/Email.php');
 require('src/php-imap-reader/EmailAttachment.php');
 require('src/php-imap-reader/Reader.php');
